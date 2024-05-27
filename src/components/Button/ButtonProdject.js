d93d9afe7dbb79db2d6f1e9b186a22b60647e3f7
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-05-27 12:42:54 +0300

    link

--- src/components/Button/ButtonProdject.js
+++ src/components/Button/ButtonProdject.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export const ButtonProdject = (link) => {
+export const ButtonProdject = ({ link }) => {
   return (
     <Link
-      href={link}
+      href={link || "#"}
       className="self-end min-w-60 w-fit mx-auto h-11 rounded-md border border-white bg-lime-500 hover:bg-lime-600 px-14 py-3 tracking-wide leading-6 text-white"
     >
       VISIT PROJECTS
