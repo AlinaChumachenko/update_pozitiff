a1a18b3382c43aeb839a6f7c8e65404a9ba1bbdb
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-05-30 11:01:45 +0300

    Update margin

--- src/components/Button/ButtonModal.js
+++ src/components/Button/ButtonModal.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const ButtonModal = ({ toggleOpen }) => {
   return (
-    <div className="flex justify-end">
+    <div className="flex justify-end mt-9 mb-32">
       <button
-        className="max-w-72 h-12 mt-9 mb-32 rounded-md bg-sky-500 hover:bg-sky-700 px-5 py-3 tracking-wide leading-6 text-white"
+        className="max-w-72 h-12 rounded-md bg-sky-500 hover:bg-sky-700 px-5 py-3 tracking-wide leading-6 text-white"
         type="button"
         onClick={() => toggleOpen()}
         width={200}
